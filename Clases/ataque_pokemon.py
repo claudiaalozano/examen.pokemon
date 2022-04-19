ad893b209c637aa66ed27f1f90ca5798b79beaaa
claudiaalozano <clau.lozano.perez@gmail.com>
2022-04-19 21:23:04 +0200

    commit

--- Clases/ataque_pokemon.py
+++ Clases/ataque_pokemon.py
@@ -1,10 +1,10 @@
 import random
-#importar clase pokemon
+
 from Clases.pokemon import Pokemon
-#importar clase tiposDePokemon
+
 from Clases.tipos_de_pokemon import tiposDePokemon
 class ataque(Pokemon):
-#funcion is_alive(self) de la clase pokemon para saber si esta vivo o no
+
 
     def is_alive(self):
         if self.puntos_salud > 0:
@@ -12,7 +12,7 @@ class ataque(Pokemon):
         else:
             return False
 
-#funcion fight_attack(self, id) de la clase pokemon para atacar a otro pokemon
+
     def fight_attack(self, id):
         if id == 1:
             self.puntos_salud -= self.indice_ataque
@@ -26,7 +26,7 @@ class ataque(Pokemon):
             self.puntos_salud -= self.indice_ataque
         elif id == 6:
             self.puntos_salud -= self.indice_ataque
-#función  fight_attack(self, Pokémon pokemon_to_attack). Método que implementa el ataque del Pokémon usando un golpe sobre otro Pokémon. Este método se basa en el método fight_defense(self, int points_of_damage) del Pokémon atacado. Se aplicará el índice de ataque del Pokémon atacante como representación del golpe dado. Este método devolverá un booleano True si se ha podido atacar a la criatura Pokémon. 
+
 
     def fight_attack(self, pokemon_to_attack):
         if pokemon_to_attack.fight_defense(self.indice_ataque):
@@ -40,7 +40,7 @@ class ataque(Pokemon):
             return False
         
 
-#funcion fight_defense(self, int points_of_damage). Este método implementa la defensa del Pokémon de un golpe de otro Pokémon. Este método actualiza el atributo de puntos de salud que tiene el Pokémon en base a los puntos de daño recibidos menos el índice de defensa del propio Pokémon. Si el índice de defensa es mayor que los puntos de ataque recibidos, el método devolverá false y el Pokémon no sufrirá ningún daño. Este método devolverá un booleano True si se ha sufrido un ataque por parte de otra criatura Pokémon, y si el pokemon es tipo aire tiene un 50% de probabilidad de que no se sufra daño.
+
     def fight_defense(self, points_of_damage):
         if self.indice_defensa > points_of_damage:
             return False
